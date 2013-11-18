c9c92c627362dbb2fa34faac8a6c687f3a75ecc2
cramja <ms267@st-andrews.ac.uk>
2013-11-18 14:47:51 +0000

    added login functionality `

--- stocktracker/urls.py
+++ stocktracker/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.views import login, logout
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'stocktracker.views.index'),
-    url(r'^testvisuals/$', 'stocktracker.views.testvisuals'),
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'stocktracker.views.index'),
+	url(r'^testvisuals/$', 'stocktracker.views.testvisuals'),
+	url(r'^admin/', include(admin.site.urls)),
+
+	url(r'^accounts/login/$',  login, ),
+	url(r'^accounts/logout/$', logout, {'template_name': 'registration/loggedout.html'}),
+	url(r'^accounts/profile/$', 'stocktracker.views.testvisuals'),
+	url(r'^accounts/register/$', 'stocktracker.views.register'),
 )
--- stocktracker/views.py
+++ stocktracker/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 
 def index(response):
 	return render(response, 'index.html')
 
 def testvisuals(response):
 	return render(response, 'testvisuals.html')
+
+def testsessions(request):
+	if request.method=='POST':
+		username = request.POST.get('username', '')
+		password = request.POST.get('password', '')
+		user = auth.authenticate(username=username, password=password)
+		if user is not None and user.is_active:
+			# Correct password, and the user is marked "active"
+			auth.login(request, user)
+			# Redirect to a success page.
+			assert False
+			return HttpResponseRedirect("/account/loggedin/")
+		else:
+			# Show an error page
+			return HttpResponseRedirect("/account/invalid/")
+	else:
+		return render(request, 'testsessions.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/") #redirects user to homepage
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", 
+    	{
+        	'form': form,
+    	})
